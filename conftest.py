b2dec686bb7879628d1344c6178817bc0e8fc75a
Harmeet Singh <er.harmeetsingh@hotmail.com>
2020-06-13 15:53:16 +0530

    Add class level fixture

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from src.DataFrame import DataFrameClass
+import os
+
+@pytest.fixture(scope='session')
+def dataframe():
+    print('Doing setup for test...')
+    data_frame = DataFrameClass(os.path.join(os.path.dirname(__file__), 'files/Alcohol.csv'))
+    yield data_frame
+    data_frame = None
+    print('Test cleanup...')
--- tests/test_data_frame.py
+++ tests/test_data_frame.py
@@ -1,17 +1,12 @@
 from src.DataFrame import DataFrameClass as DataFrameClass
 
-def test_data_frame_read():
-    data_frame = DataFrameClass('http://vincentarelbundock.github.io/Rdatasets/csv/mosaicData/Alcohol.csv')
-    assert data_frame is not None
+def test_data_frame_read(dataframe):
+    assert dataframe is not None
 
 
-def test_filter_column_on_regex():
-    data_frame = DataFrameClass('http://vincentarelbundock.github.io/Rdatasets/csv/mosaicData/Alcohol.csv')
-    assert len(data_frame.list_columns()) > len(data_frame.filter_out_columns_regex('[a]').list_columns())
+def test_filter_column_on_regex(dataframe):
+    assert len(dataframe.list_columns()) > len(dataframe.filter_out_columns_regex('[a]').list_columns())
 
 
-def test_filter_column_on_value():
-    data_frame = DataFrameClass('http://vincentarelbundock.github.io/Rdatasets/csv/mosaicData/Alcohol.csv')
-    assert data_frame.filter_out_column_on_value('country','India').get_record_count() == 2
-
-
+def test_filter_column_on_value(dataframe):
+    assert dataframe.filter_out_column_on_value('country', 'India').get_record_count() == 2
